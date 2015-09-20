bb90abb3ddd4330548e48c3933e25dac6563edb6
psycho <priebeman@googlemail.com>
2015-09-20 16:58:08 +0200

    add oauth2

--- main.go
+++ main.go
@@ -1,27 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"gotank/libs/yaml"
 	"io/ioutil"
 	"log"
+
+	"golang.org/x/oauth2"
 )
 
 type config struct {
-	Download string
+	Clientid string
+	Secret   string
 	DownDir  string
-	Upload   string
 	UpDir    string
 }
 
 func main() {
 	c := config{}
 	readYaml("config.yml", &c)
-	down := Onedrive{c.Download}
-	up := Onedrive{c.Upload}
-	//fmt.Println(down.Children(c.DownDir))
-	// fmt.Println(up.Children(c.UpDir))
 
+	downToken := getToken(c.Clientid, c.Secret, "Download")
+	upToken := getToken(c.Clientid, c.Secret, "Upload")
+
+	down := Onedrive{downToken}
+	up := Onedrive{upToken}
 	down.SyncWith(up, c.DownDir, c.UpDir)
+}
+
+func getToken(clientid, secret, msg string) string {
+	conf := &oauth2.Config{
+		ClientID:     clientid,
+		ClientSecret: secret,
+		Scopes:       []string{"onedrive.readwrite"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://login.live.com/oauth20_authorize.srf",
+			TokenURL: "https://login.live.com/oauth20_token.srf",
+		},
+	}
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for %s: %v", msg, url)
+	fmt.Print("\nCode:")
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+	tok, err := conf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tok.AccessToken
 }
 
 func readYaml(filename string, data interface{}) {
--- onedrive.go
+++ onedrive.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
@@ -32,21 +33,21 @@ func (o Onedrive) submit(s string) (items []Item) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", api+s, nil)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	req.Header.Add("Authorization", "bearer "+o.Token)
 	resp, err := client.Do(req)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	jsonParsed, err := gabs.ParseJSON(body)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
 	children, _ := jsonParsed.S("value").Children()
@@ -76,7 +77,6 @@ func (o Onedrive) Children(path string) []Item {
 }
 
 func (o Onedrive) Mkdir(path string) {
-
 }
 
 func (o Onedrive) SyncWith(up Onedrive, downDir, upDir string) {
@@ -85,57 +85,64 @@ func (o Onedrive) SyncWith(up Onedrive, downDir, upDir string) {
 		return
 	}
 	up.Mkdir(upDir)
+
+	chunk := 1024 * 1024 * 10
+	buffer := make([]byte, chunk)
+	var b bytes.Buffer
+	var size int64
+
 	for _, item := range items {
 		if item.Folder {
-			fmt.Println("create directory and call SyncWith")
+			fmt.Println("Todo: Create directory and call SyncWith")
 		} else {
-
 			client := &http.Client{}
 			req, err := http.NewRequest("GET", item.Link, nil)
 			if err != nil {
-				return
+				log.Fatal(err)
 			}
 			req.Header.Add("Authorization", "bearer "+o.Token)
 			resp, err := client.Do(req)
 			if err != nil {
-				return
+				log.Fatal(err)
 			}
-			defer resp.Body.Close()
 
 			uploadUrl := up.createSession(item.Name, upDir)
 			if len(uploadUrl) == 0 {
-				return
+				log.Fatal("No Upload-Session")
 			}
 			fmt.Println("Upload:", item.Name)
 
-			chunk := 1024 * 1024 * 10
-			buffer := make([]byte, chunk)
-			var size int64
+			size = 0
 			for {
 				num, err := io.ReadAtLeast(resp.Body, buffer, chunk)
 
-				var b bytes.Buffer
+				b.Reset()
 				b.Write(buffer)
 				b.Truncate(num)
 				req, err2 := http.NewRequest("PUT", uploadUrl, &b)
 				if err2 != nil {
-					break
+					log.Fatal(err2)
 				}
 
 				r := fmt.Sprintf("bytes %d-%d/%d", size, size+int64(num)-1, item.Size)
 				req.Header.Add("Authorization", "bearer "+up.Token)
 				req.Header.Add("Content-Length", fmt.Sprintf("%d", num))
 				req.Header.Add("Content-Range", r)
-				res, err3 := client.Do(req)
-				if err3 != nil {
-					break
+				res, err2 := client.Do(req)
+				if err2 != nil {
+					log.Fatal(err2)
 				}
+				res.Body.Close()
 				size += int64(num)
 				fmt.Println(size, "from", item.Size, "Status", res.StatusCode)
-				if err != nil || res.StatusCode > 300 {
+				if err != nil {
 					break
 				}
+				if res.StatusCode >= 400 {
+					log.Fatal("Upload-Error")
+				}
 			}
+			resp.Body.Close()
 		}
 	}
 }
@@ -145,20 +152,20 @@ func (o Onedrive) createSession(name, dir string) (url string) {
 	uri := api + "/drive/root:" + dir + "/" + name + ":/upload.createSession"
 	req, err := http.NewRequest("POST", uri, nil)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	req.Header.Add("Authorization", "bearer "+o.Token)
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	result, err := ioutil.ReadAll(resp.Body)
 
 	jsonParsed, err := gabs.ParseJSON(result)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
 	url, _ = jsonParsed.Path("uploadUrl").Data().(string)
@@ -173,14 +180,14 @@ func (o Onedrive) RemoteDownload(post, uri, name string) {
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", post, body)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	req.Header.Add("Authorization", "bearer "+o.Token)
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Prefer", "respond-async")
 	resp, err := client.Do(req)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	status, err := ioutil.ReadAll(resp.Body)
